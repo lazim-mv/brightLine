4415fea61174631c6aca748e4e7458aaca2c1c8c
lazim mv <lazimlatheef@gmail.com>
2024-01-07 02:57:42 +0530

    0.0.1

--- src/app/components/Carousel.js
+++ src/app/components/Carousel.js
@@ -1,31 +1,31 @@
-import React from "react";
+// import React from "react";
 
-const Carousel = ({images, currentIndex,}) => {
-  return (
-    <div className="carouselContainer">
-      <div className="carousel">
-        <div
-          className="imageContainer"
-          style={{
-            transform: `translateX(-${currentIndex * 32.544270833333336}vw)`,
-          }}
-        >
-          {images.map((image, index) => (
-            <div className="relative" key={index}>
-              <Image
-                width={377}
-                height={420}
-                src={image}
-                alt={`Image ${index + 1}`}
-                className={index === currentIndex ? "visible" : "hidden"}
-              />
-              <div className="imageFrame"></div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+// const Carousel = ({images, currentIndex,}) => {
+//   return (
+//     <div className="carouselContainer">
+//       <div className="carousel">
+//         <div
+//           className="imageContainer"
+//           style={{
+//             transform: `translateX(-${currentIndex * 32.544270833333336}vw)`,
+//           }}
+//         >
+//           {images.map((image, index) => (
+//             <div className="relative" key={index}>
+//               <Image
+//                 width={377}
+//                 height={420}
+//                 src={image}
+//                 alt={`Image ${index + 1}`}
+//                 className={index === currentIndex ? "visible" : "hidden"}
+//               />
+//               <div className="imageFrame"></div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Carousel;
+// export default Carousel;
