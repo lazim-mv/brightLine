af1aa2254e703d876aa8d225f0095e19b258ff9c
lazim mv <lazimlatheef@gmail.com>
2024-01-11 22:18:40 +0530

    v0.0.4 aboutPage

--- src/app/pages/About/page.js
+++ src/app/pages/About/page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../Styles/aboutus.module.css";
+import styles from "../../styles/aboutus.module.css";
 import {
   aboutPageContainer1,
   aboutPageContainer2,
