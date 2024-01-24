a4bd496d2e16eb7bd14d8e7d112004c074237c8c
lazim mv <lazimlatheef@gmail.com>
2024-01-24 09:13:47 +0530

    0.0.9 carouselIndicators

--- src/app/pages/About/page.js
+++ src/app/pages/About/page.js
@@ -24,54 +24,80 @@ const page = () => {
     <>
       <Header />
       <MobileHeader />
-      <div className={styles.container1}>
-        <SectionName
-          sectionText={aboutPageContainer1.sectionName}
-          dashText={true}
-          textAllign="center"
-        />
-        <SectionTitle
-          sectionText={aboutPageContainer1.sectionTitle}
-          textAllign="center"
-        />
-        <SectionDescription
-          sectionText={aboutPageContainer1.description}
-          textAllign="center"
-          width="65.88541666666666vw"
-          margin="0 auto"
-        />
-        <div className={styles.aboutBannerImageContainer}>
-          <Image
-            className=""
-            src={aboutPageContainer1.img}
-            alt="Asian Engineer"
-            quality={100}
-            priority={true}
-            unoptimized
-            width={100}
-            height={100}
-          />
-        </div>
-      </div>
 
-      <div className={styles.container2}>
-      <SectionTitle 
-        sectionText={aboutPageContainer2.sectionTitle}
-        textAllign='center'
-        width="60.611979166666664vw"
-      />
-        <div className={styles.cardContainer}>
-          {cardData.map((data, index) => (
-            <div
-              className={`${styles[`aboutItem${index + 1}`]} ${
-                styles.aboutItem
-              }`}
-              key={index}
-            >
-              <div className={styles.aboutImage}>
+      <div className={styles.mainContainer1}>
+        <div className={styles.container1}>
+          <SectionName
+            sectionText={aboutPageContainer1.sectionName}
+            dashText={true}
+            textAllign="center"
+          />
+          <SectionTitle
+            sectionText={aboutPageContainer1.sectionTitle}
+            textAllign="center"
+          />
+          <SectionDescription
+            sectionText={aboutPageContainer1.description}
+            textAllign="center"
+            width="65.88541666666666vw"
+            margin="0 auto"
+          />
+          <div className={styles.aboutBannerImageContainer}>
+            <Image
+              className=""
+              src={aboutPageContainer1.img}
+              alt="Asian Engineer"
+              quality={100}
+              priority={true}
+              unoptimized
+              width={100}
+              height={100}
+            />
+          </div>
+        </div>
+
+        <div className={styles.container2}>
+          <SectionTitle
+            sectionText={aboutPageContainer2.sectionTitle}
+            textAllign="center"
+            width="60.611979166666664vw"
+          />
+          <div className={styles.cardContainer}>
+            {cardData.map((data, index) => (
+              <div
+                className={`${styles[`aboutItem${index + 1}`]} ${
+                  styles.aboutItem
+                }`}
+                key={index}
+              >
+                <div className={styles.aboutImage}>
+                  <Image
+                    className="cardIcon"
+                    src={data.img}
+                    alt="Asian Engineer"
+                    width={100}
+                    height={100}
+                    quality={100}
+                    priority={true}
+                    unoptimized
+                  />
+                </div>
+                <div className={styles.container2Contents}>
+                  <h3>{data.title}</h3>
+                  <SectionDescription sectionText={data.description} />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.container3}>
+          <div className={styles.vissionMissionContainer}>
+            <div className={styles.mission}>
+              <div>
                 <Image
-                  className="cardIcon"
-                  src={data.img}
+                  className={styles.vissionMissionImage}
+                  src={aboutPageContainer3[0].img}
                   alt="Asian Engineer"
                   width={100}
                   height={100}
@@ -80,103 +106,79 @@ const page = () => {
                   unoptimized
                 />
               </div>
-              <div className={styles.container2Contents}>
-                <h3>{data.title}</h3>
-                <SectionDescription sectionText={data.description} />
+              <div className={styles.vissionMissionContent}>
+                <SectionTitle sectionText={aboutPageContainer3[0].title} />
+                <SectionDescription
+                  sectionText={aboutPageContainer3[0].description}
+                />
               </div>
             </div>
-          ))}
-        </div>
-      </div>
-
-      <div className={styles.container3}>
-        <div className={styles.vissionMissionContainer}>
-          <div className={styles.mission}>
-            <div>
-              <Image
-                className={styles.vissionMissionImage}
-                src={aboutPageContainer3[0].img}
-                alt="Asian Engineer"
-                width={100}
-                height={100}
-                quality={100}
-                priority={true}
-                unoptimized
-              />
-            </div>
-            <div className={styles.vissionMissionContent}>
-              <SectionTitle sectionText={aboutPageContainer3[0].title} />
-              <SectionDescription
-                sectionText={aboutPageContainer3[0].description}
-              />
-            </div>
-          </div>
-          <div className={styles.vission}>
-            <div className={styles.vissionMissionContent}>
-              <SectionTitle sectionText={aboutPageContainer3[1].title} />
-              <SectionDescription
-                sectionText={aboutPageContainer3[1].description}
-              />
-            </div>
-            <div>
-              <Image
-                className={styles.vissionMissionImage}
-                src={aboutPageContainer3[1].img}
-                alt="Asian Engineer"
-                width={100}
-                height={100}
-                quality={100}
-                priority={true}
-                unoptimized
-              />
+            <div className={styles.vission}>
+              <div className={styles.vissionMissionContent}>
+                <SectionTitle sectionText={aboutPageContainer3[1].title} />
+                <SectionDescription
+                  sectionText={aboutPageContainer3[1].description}
+                />
+              </div>
+              <div>
+                <Image
+                  className={styles.vissionMissionImage}
+                  src={aboutPageContainer3[1].img}
+                  alt="Asian Engineer"
+                  width={100}
+                  height={100}
+                  quality={100}
+                  priority={true}
+                  unoptimized
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className={styles.container4}>
-        <SectionTitle
-          sectionText={aboutPageContainer4.sectionTitle}
-          textAllign="center"
-        />
-        <div className={styles.profilesContainer}>
-          {aboutPageContainer4.imageCard.map((data, index) => (
-            <div className={styles.profileContainer} key={index}>
-              <Image
-                className=""
-                src={data.img}
-                alt="Asian Engineer"
-                quality={100}
-                priority={true}
-                unoptimized
-                width={100}
-                height={100}
-              />
-              <h3>{data.name}</h3>
-              <SectionDescription
-                sectionText={data.position}
-                textAllign="center"
-              />
-            </div>
-          ))}
+        <div className={styles.container4}>
+          <SectionTitle
+            sectionText={aboutPageContainer4.sectionTitle}
+            textAllign="center"
+          />
+          <div className={styles.profilesContainer}>
+            {aboutPageContainer4.imageCard.map((data, index) => (
+              <div className={styles.profileContainer} key={index}>
+                <Image
+                  className=""
+                  src={data.img}
+                  alt="Asian Engineer"
+                  quality={100}
+                  priority={true}
+                  unoptimized
+                  width={100}
+                  height={100}
+                />
+                <h3>{data.name}</h3>
+                <SectionDescription
+                  sectionText={data.position}
+                  textAllign="center"
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.container5}>
+          <SectionTitle
+            sectionText={aboutPageContainer5.sectionTitle}
+            textAllign="center"
+          />
+          <div className={styles.faculties}>
+            {aboutPageContainer5.imageCard.map((data, index) => (
+              <div className={styles.faculty} key={index}>
+                <h3>{data.name}</h3>
+                <SectionDescription sectionText={data.position} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-
-      <div className={styles.container5}>
-        <SectionTitle
-          sectionText={aboutPageContainer5.sectionTitle}
-          textAllign="center"
-        />
-        <div className={styles.faculties}>
-          {aboutPageContainer5.imageCard.map((data, index) => (
-            <div className={styles.faculty} key={index}>
-              <h3>{data.name}</h3>
-              <SectionDescription sectionText={data.position} />
-            </div>
-          ))}
-        </div>
-      </div>
-
       <ContactBanner />
       <Footer />
     </>
